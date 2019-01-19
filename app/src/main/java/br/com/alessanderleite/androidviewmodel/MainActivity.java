c003eed98626ff1ec171935f697970d85e378fd3
alessanderleite <alessander.logistica@gmail.com>
2019-01-19 12:42:16 -0200

    declare the used objects inside the MainActivity class

--- app/src/main/java/br/com/alessanderleite/androidviewmodel/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/androidviewmodel/MainActivity.java
@@ -2,12 +2,25 @@ package br.com.alessanderleite.androidviewmodel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    HeroesAdapter adapter;
+
+    List<Hero> heroList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
