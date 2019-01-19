7742b43efde183ac45cc67b61a98464761362ef4
alessanderleite <alessander.logistica@gmail.com>
2019-01-19 12:34:26 -0200

    code the HeroesAdapter class

--- app/src/main/java/br/com/alessanderleite/androidviewmodel/HeroesAdapter.java
+++ app/src/main/java/br/com/alessanderleite/androidviewmodel/HeroesAdapter.java
@@ -1,4 +1,61 @@
 package br.com.alessanderleite.androidviewmodel;
 
-public class HeroesAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class HeroesAdapter extends RecyclerView.Adapter<HeroesAdapter.HeroViewHolder> {
+
+    Context context;
+    List<Hero> heroList;
+
+    public HeroesAdapter(Context context, List<Hero> heroList) {
+        this.context = context;
+        this.heroList = heroList;
+    }
+
+    @NonNull
+    @Override
+    public HeroViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_layout, viewGroup, false);
+        return new HeroViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HeroViewHolder holder, int position) {
+        Hero hero = heroList.get(position);
+
+        Glide.with(context)
+                .load(hero.getImageurl())
+                .into(holder.imageView);
+
+        holder.textView.setText(hero.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return heroList.size();
+    }
+
+    public class HeroViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+
+        public HeroViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            textView = (TextView) itemView.findViewById(R.id.textView);
+        }
+    }
 }
